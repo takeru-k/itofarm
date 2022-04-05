6e169ccc1bcc50d3be4b7d7c88fd0875f8bb7334
takeru-k <kam41893@gmail.com>
2022-04-06 07:18:25 +0900

    ファイルを追加

---
+++ js/main.js
@@ -0,0 +1,4 @@
+$(".header__button__line").click(function () {//ボタンがクリックされたら
+  $(this).toggleClass('active');//ボタン自身に activeクラスを付与し
+    $("#g-nav").toggleClass('panelactive');//ナビゲーションにpanelactiveクラスを付与
+});
